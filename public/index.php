d0c8a2af89882ddcda3c2fa7c07a96b90ab1c901
baur114 <baurdotnet@gmail.com>
2019-01-01 13:44:06 +0600

    add handler

--- public/index.php
+++ public/index.php
@@ -1,19 +1,41 @@
 <?php
 
-// Подключение автозагрузки через composer
+namespace App;
+
 require __DIR__ . '/../vendor/autoload.php';
 
-// Вывод ошибок на экран (для удобной отладки)
+$faker = \Faker\Factory::create();
+$faker->seed(1234);
+
+$domains = [];
+for ($i = 0; $i < 10; $i++) {
+	    $domains[] = $faker->domainName;
+}
+
+$phones = [];
+for ($i = 0; $i < 10; $i++) {
+	    $phones[] = $faker->phoneNumber;
+}
+
 $configuration = [
-     'settings' => [
-             'displayErrorDetails' => true,
-     ],
-];
+	    'settings' => [
+		            'displayErrorDetails' => true,
+			        ],
+			];
 
 $app = new \Slim\App($configuration);
 
 $app->get('/', function ($request, $response) {
-	    return $response->write('Welcome to Slim!');
+	    $response->write('go to the /phones or /domains');
 });
+
+$app->get('/phones', function ($request, $response) use($phones) {
+	    $response->write(json_encode($phones));
+});
+
+$app->get('/domains', function ($request, $response) use($domains) {
+	    $response->write(json_encode($domains));
+});
+
 $app->run();
 
